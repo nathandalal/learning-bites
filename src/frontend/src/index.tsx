4ff8202ca71e09b864d8e94d6fe50cafe176e1ec
nathan dalal <nathanhdalal@gmail.com>
2023-03-25 16:44:58 -0700

    add routed frontend with typescript

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from './App';
+import 'index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
