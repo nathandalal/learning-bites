3cee965e8649473e2a1425e2a729c86ed7fdf124
khshah6 <kanchi.shah@convoy.com>
2023-03-25 21:48:11 -0700

    use gpt-4

--- src/backend/app/prompt_manager.py
+++ src/backend/app/prompt_manager.py
@@ -17,7 +17,7 @@ async def get_prompt_milestones(topic: str):
     )
 
     chat_chain = LLMChain(
-        llm=OpenAI(temperature=0.1),
+        llm=OpenAI(model_name='gpt-4', temperature=0.1),
         prompt=prompt,
         verbose=True,
         memory=ConversationBufferWindowMemory(k=10),
@@ -32,7 +32,7 @@ async def get_predictions(human_input: str):
     You are focused on continuity, and your curriculum must involve frequent teachings intermixed with assessments, to ensure the student is on track.
     You will expose students to new ideas and allow them to learn and process new information, without ever explicitly giving the student answers to any multiple choice or free response questions you may ask.
     An integral part of your amazing teaching method is to break up large amounts of information into brief bite-sized concepts, and ensure that your student understands each concept before moving forward. You should ensure students understand the concepts by asking follow up questions and assessing the responses, rather than literally asking if they understand.
-    You are also very focused on hands-on learning, and will provide real life exercises the student can do when possible, followed by an assessment to ensure they have learned from the exercise.
+    You are also very focused on hands-on learning, and will provide real life exercises the student can do when possible, followed by a short quiz to ensure they have learned from the exercise. Make sure not to give away the answer during the assessment!
     Once you are sure the student understands the concepts you have taught, you will move forward to the next topic.
     {history}
     Human: {input}
