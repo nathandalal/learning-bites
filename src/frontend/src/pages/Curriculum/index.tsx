679a23dab0966672899d09d3837a19a9274863e6
nathan dalal <nathanhdalal@gmail.com>
2023-03-25 19:42:39 -0700

    fix

--- src/frontend/src/pages/Curriculum/index.tsx
+++ src/frontend/src/pages/Curriculum/index.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
-
-=======
->>>>>>> 6ddd08b85d28061f621dafea7563aaefdecc5c18
 import logo from '../../logo.svg';
 import './style.css';
 import { Link, useParams } from 'react-router-dom';
@@ -13,7 +9,6 @@ import Sidebar from "../../components/Sidebar";
 
 
 export function Curriculum() {
-<<<<<<< HEAD
   const { milestone } = useParams<{ milestone: string }>();
   const [learningAnswerText, setLearningAnswerText] = useState("");
   const [backendResponse, setBackendResponse] = useState<string | null>(null);
@@ -55,46 +50,8 @@ export function Curriculum() {
        <p className="py-2">
          You're learning about the following topic: {milestone}
        </p>
-<<<<<<< Updated upstream
 
-=======
- 
-       <h3 className="py-2">
-         Learning Goal 1 
-       </h3>
-=======
-   const [learningAnswerText] = useState("");
-   const [backendResponse, setBackendResponse] = useState<string | null>(null);
->>>>>>> 6ddd08b85d28061f621dafea7563aaefdecc5c18
-
-   const fetchBackendResponse = async () => {
-      try {
-         const response = await axiosClient({
-            method: 'post',
-            url: `/response`,
-            params: {
-               human_input: learningAnswerText,
-            },
-            data: {},
-         })
-
-         setBackendResponse(response.data.data);
-      } catch (error) {
-         console.error("Error fetching backend response:", error);
-      }
-   };
-
-   useEffect(() => {
-      fetchBackendResponse();
-   }, []);
-
-   return (
-      <div className="container mx-auto py-5">
-         <p className="py-2">
-            Here is a curriculum tailored to your learning needs.
-         </p>
-
->>>>>>> Stashed changes
+       
          {backendResponse && (
             <div className="py-2">
                {backendResponse}
